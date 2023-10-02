e9c508642bea02f82996df1b7ffdb6ebf75e8a8f
sharif <sharifkhanxyz@gmail.comm>
2023-10-02 10:37:29 -0700

    java-script

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var x=20;
+var y=60;
+z=x*y;
+
+var s=120;
+var z=100;
+d=s+z;
+
+
+
+
+let phone=8;
+let number=8;
+
+
+
+document.getElementById('html').innerHTML =z;
+document.getElementById('html1').innerHTML=d;
+document.getElementById('html1').style.color='green';
+document.getElementById('html').style.fontSize='50px';
+document.getElementById('html1').style.fontSize='50px';
+
+
+
+document.getElementById('html2').innerHTML =(phone == number);
